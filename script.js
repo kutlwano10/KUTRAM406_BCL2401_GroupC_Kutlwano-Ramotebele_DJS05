340c2eb5099749d7adaf0a73ee0bb01a20244027
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-05-23 16:54:51 +0200

    dispatched and consoled getState() to get the current state

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const createStore = (initial, reducer) => {
   };
 
   /**
-   *
+   * Takes the an Action as a param
    * @param {*} action
    */
   const dispatch = (action) => {
@@ -39,5 +39,21 @@ const reducer = (state = 0, action, actions) => {
   return found.callback(state);
 };
 
+/**
+ * @constant {Object} store - is an object of functions
+ */
 const store = createStore(0, reducer);
+
+store.subscribe({type : 'Add'},  state => state + 1)
+store.subscribe({type : 'Subtract'},  state => state - 1)
+store.subscribe({type : 'Reset'},  () => 0)
+
+store.dispatch({type : "Add"})
+console.log(store.getState())
+store.dispatch({type : "Add"})
+console.log(store.getState())
+store.dispatch({type : "Subtract"})
+console.log(store.getState())
+store.dispatch({type : "Reset"})
+console.log(store.getState())
 
